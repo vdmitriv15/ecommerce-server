caa6fd460dbd55762097ea56260ca37a5153685b
Dmitry Vylegzhanin <vdmitriv15@gmail.com>
2021-06-30 15:56:27 -0400

    added __str__

--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -14,4 +17,5 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products_images', blank=True)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
