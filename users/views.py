d6c6ccf781f318e3b03caeded13834dee4b99777
Dmitry Vylegzhanin <vdmitriv15@gmail.com>
2021-07-07 18:27:54 -0400

    added auth and validate

--- users/views.py
+++ users/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+from users.forms import UserLoginForm
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
+    context = {
+        'title': 'GeekShop - Authorisation',
+        'form': form,
+    }
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    context = {
+        'title': 'GeekShop - Registration',
+    }
+    return render(request, 'users/registration.html', context)
