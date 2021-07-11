4a09f98cadd2bb9472b7b3bb2f1dab6d6d346f7e
Dmitry Vylegzhanin <vdmitriv15@gmail.com>
2021-07-11 19:37:02 -0400

    added cart

--- carts/views.py
+++ carts/views.py
@@ -9,9 +9,8 @@ def cart_add(request, product_id):
 
     if not carts.exists():
         Cart.objects.create(user=request.user, product=product, quantity=1)
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     else:
         cart = carts.first()
         cart.quantity += 1
         cart.save()
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
