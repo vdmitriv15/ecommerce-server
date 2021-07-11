f21f7a0173f0a18bebbac5a2efbd15944ead9812
Dmitry Vylegzhanin <vdmitriv15@gmail.com>
2021-07-11 12:58:49 -0400

    added form to profile

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -45,7 +45,9 @@ def logout(request):
 
 
 def profile(request):
+    form = UserProfileForm(instance=request.user)
     context = {
         'title': 'GeekShop - Profile',
+        'form': form
     }
     return render(request, 'users/profile.html', context)
