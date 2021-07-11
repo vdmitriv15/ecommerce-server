4ce373aadc383020f3966d69c87cf4510c7f8155
Dmitry Vylegzhanin <vdmitriv15@gmail.com>
2021-07-11 12:43:32 -0400

    created template for profile

--- users/views.py
+++ users/views.py
@@ -42,3 +42,10 @@ def registration(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+def profile(request):
+    context = {
+        'title': 'GeekShop - Profile',
+    }
+    return render(request, 'users/profile.html', context)
