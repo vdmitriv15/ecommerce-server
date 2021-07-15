75b49a2fba2c592b10182b1f9d76aee64be9f463
Dmitry Vylegzhanin <vdmitriv15@gmail.com>
2021-07-15 12:44:17 -0400

    added Django admin for users

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from users.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email')
+    fields = (('first_name', 'last_name'), ('username', 'email'), 'image')
+    ordering = ('username',)
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+
